e0421798ccc1162c5e5ff3a1dd27eb94f227f26f
Jflores-2019424 <jflores-2019424@kinal.edu.gt>
2023-10-19 15:54:49 -0600

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,22 @@ function App() {
      {/* Banner inicial */}
 
       <div id="banner">
-        <p class="bn_t1">¡ENTRENA CON NOSOTROS!</p>
-        <p class="bn_t2"><a href="#">Aplicar aquí</a> y haz realidad tu sueño de ser el Techie o Digital Marketer que has
+        <p className="bn_t1">¡ENTRENA CON NOSOTROS!</p>
+        <p className="bn_t2"><a href="#">Aplicar aquí</a> y haz realidad tu sueño de ser el Techie o Digital Marketer que has
           soñado.</p>
       </div>
 
      {/* Navbar Level Up */}
       <section id="menu">
-        <a class="logo" href="#">
+        <a className="logo" href="#">
           <img src="https://levelup.gt/img/logo_negativo.png" />
         </a>
         <section id="nav">
-          <a href="#" class="button active">Programas</a>
-          <a href="#" class="button">Ser mentor</a>
-          <a href="#" class="button">¿Por qué level up?</a>
-          <a href="#" class="button">Contacto</a>
-          <a href="#" class="button hot">Aplicar</a>
+          <a href="#" className="button active">Programas</a>
+          <a href="#" className="button">Ser mentor</a>
+          <a href="#" className="button">¿Por qué level up?</a>
+          <a href="#" className="button">Contacto</a>
+          <a href="#" className="button hot">Aplicar</a>
         </section>
       </section>
 
@@ -32,12 +32,12 @@ function App() {
 
     
 
-      <div class="container text-center">
+      <div className="container text-center">
         <br />
       <header> <strong className='letter-font'> <h1>Generacion Autimatizada de Contenido Académico</h1></strong></header>
       <br />
-        <div class="row">
-          <div class="col">
+        <div className="row">
+          <div className="col">
             <br />
             <div>
               <label className='letter-font' htmlFor="">¿Que deseas primero?</label> <br />
@@ -60,12 +60,12 @@ function App() {
               </select>
             </div>
           </div>
-          <div class="col">        
+          <div className="col">        
             <input type="text" className='letter-font form-control' placeholder='Escribe tu tema ...' /> <br />
             <button type="button" className="letter-font position-absolute bottom-0 start-50 translate-middle-x generate btn btn-danger">Generar</button>
           </div>
           
-          <div class="col">
+          <div className="col">
           <br />
           <label className='letter-font' htmlFor="">¿Cuantos ejercicios practicos desea?</label> <br />
             <select className='form-select' name="" id="">
@@ -86,6 +86,56 @@ function App() {
           </div>
         </div>
       </div>
+      <footer>
+      <figure className="logo_foot">
+            <img src="" />
+        </figure>
+        <section className="info mx-5">
+            <section className="secciones grow">
+                <p>SECCIONES</p>
+                <a href="">
+                    <p>¿Quiénes somos?</p>
+                </a>
+                <a href="">
+                    <p>¿Qué hacemos?</p>
+                </a>
+                <a href="">
+                    <p>Términos y condiciones</p>
+                </a>
+            </section>
+            <section className="informacion grow">
+                <p>INFORMACIÓN</p>
+                <section className="nav-info">
+                    <a href="index.html">
+                        <p>Level up</p>
+                    </a>
+                    <a href="index.html#programas">
+                        <p>Programas</p>
+                    </a>
+                    <a href="index.html#contacto">
+                        <p>Contacto</p>
+                    </a>
+                    <a href="form.html">
+                        <p>Aplicar</p>
+                    </a>
+                    <a href="mentor.html">
+                        <p>Ser mentor</p>
+                    </a>
+                    <a href="index.html#vovacion">
+                        <p>¿Por qué Level up?</p>
+                    </a>
+                </section>
+            </section>
+            <section className="redes grow">
+                <p>SIGUENOS EN</p>
+                <div className="sm_nav">
+                    <a href="https://www.facebook.com/somoslevelup/"><i className="fa-brands fa-facebook"></i></a>
+                    <a href="https://www.linkedin.com/company/somoslevelup"><i className="fa-brands fa-linkedin"></i></a>
+                    <a href="https://instagram.com/somos_levelup?igshid=YmMyMTA2M2Y="><i className="fa-brands fa-instagram"></i></a>
+                </div>
+            </section>
+          </section>
+      </footer>
     </> 
   )
 }
